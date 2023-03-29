f23f572bf282460fe8cbbaf931a5bee571d81bf1
Clay <thejourney318@gmail.com>
2023-03-28 20:20:19 -0400

    Provoking a commit.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'LaravelRRR';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'LaravelRRR'
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
