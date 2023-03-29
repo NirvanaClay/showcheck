4eb27371112b84a3373950f31154a27c857a44f6
Clay <thejourney318@gmail.com>
2023-03-29 00:22:15 -0400

    Update vite.config.js with correct import statement

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // vite.config.js
 // import { defineConfig } from 'vite'
@@ -12,7 +13,7 @@ export default defineConfig(({ command }) => ({
 //   ],
 resolve: {
     alias: {
-      '@': resolve(__dirname, 'resources/js'),
+      '@': path.resolve(__dirname, 'resources/js'),
     },
   },
   build: {
