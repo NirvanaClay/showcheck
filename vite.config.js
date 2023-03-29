28886923d9610baba7a585e46ecd262f156fb8bd
Clay <thejourney318@gmail.com>
2023-03-29 01:46:21 -0400

    Trying original vite.config.js again.

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,36 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel(['resources/js/Pages/Main.jsx']),
-//         react(),
-//     ],
-//     build: {
-//         target: 'esnext'
-//     }
-// });
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-// Check if the environment is production
-const isProduction = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  plugins: [
-    laravel(['resources/js/Pages/Main.jsx']),
-    react(),
-  ],
-  build: {
-    target: 'esnext',
-    outDir: 'public',
-    manifest: true,
-    rollupOptions: {
-      input: 'resources/js/app.jsx',
-    },
-  },
-  base: isProduction ? '/public/' : '/',
+    plugins: [
+        laravel(['resources/js/Pages/Main.jsx']),
+        react(),
+    ],
+    build: {
+        target: 'esnext'
+    }
 });
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import react from '@vitejs/plugin-react';
+
+// // Check if the environment is production
+// const isProduction = process.env.NODE_ENV === 'production';
+
+// export default defineConfig({
+//   plugins: [
+//     laravel(['resources/js/Pages/Main.jsx']),
+//     react(),
+//   ],
+//   build: {
+//     target: 'esnext',
+//     outDir: 'public',
+//     manifest: true,
+//     rollupOptions: {
+//       input: 'resources/js/app.jsx',
+//     },
+//   },
+//   base: isProduction ? '/public/' : '/',
+// });
