9d8bd130a18882558d63ef3f879f8bca4993f22a
Clay <thejourney318@gmail.com>
2023-03-30 15:44:57 -0400

    Triggering deploy.

--- resources/js/Components/LoginForm.jsx
+++ resources/js/Components/LoginForm.jsx
@@ -16,7 +16,7 @@ const LoginForm = ({ setLoginStatus, setUser, loginStatus, passwordVisibility, s
   function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    if (parts.length === 2) return parts.pop().split(';').shift()
   }
   
   const loginUser = async (e) => {
