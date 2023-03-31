a668ac08f099cea2afd04bfe824182039a1d2976
Clay <thejourney318@gmail.com>
2023-03-31 17:10:22 -0400

    Fixed difference between this and expense-tracker vite.config.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        laravel(['resources/js/Pages/Main.jsx']),
+        laravel(['resources/js/app.jsx']),
         react(),
     ],
     build: {
