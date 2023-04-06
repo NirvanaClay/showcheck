d3c9cc7f8490876f40cb7da1d2d9489ca0dd0187
Clay <thejourney318@gmail.com>
2023-04-06 16:54:32 -0400

    Forcing deploy.

--- resources/js/axiosConfig.js
+++ resources/js/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'https://showcheck.herokuapp.com';
 
 export default axios;
